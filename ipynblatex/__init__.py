bd8547d282737b81d71f06a1796af0fabe644fdc
shikijiritsu <shikijiritsu@yahoo.co.jp>
2017-11-12 11:00:25 +0900

    add tests

---
+++ ipynblatex/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+'''ipynblatex
+use with (...env...)/Lib/site-packages/ipynbutl/__init__.py
+--------
+import sys, os
+from notebook import notebookapp
+
+wd = os.getcwd()
+for svr in notebookapp.list_running_servers():
+  nd = svr['notebook_dir']
+  if wd.startswith(nd):
+    sys.path.append(nd)
+    break
+--------
+'''
+
+__version__ = '0.0.1'
