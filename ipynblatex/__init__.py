9d007209a449acfa551e03f948273da563fd06d0
shikijiritsu <shikijiritsu@yahoo.co.jp>
2017-11-18 12:09:29 +0900

    ipynbutl.ipynb_dir

--- ipynblatex/__init__.py
+++ ipynblatex/__init__.py
@@ -6,13 +6,15 @@ use with (...env...)/Lib/site-packages/ipynbutl/__init__.py
 import sys, os
 from notebook import notebookapp
 
+ipynb_dir = None
 wd = os.getcwd()
 for svr in notebookapp.list_running_servers():
   nd = svr['notebook_dir']
   if wd.startswith(nd):
     sys.path.append(nd)
+    ipynb_dir = nd
     break
 --------
 '''
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
